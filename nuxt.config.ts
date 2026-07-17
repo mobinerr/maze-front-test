d65392dfb778cb03ccdc4e369f5760713e7e8cb8
mobinerr <mobinerr@gmail.com>
2026-07-17 16:49:35 +0330

    build(app): install core frontend dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@pinia/nuxt', '@vueuse/nuxt'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
