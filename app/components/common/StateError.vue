48b2b690b7872ea287425444ada8cf377b02b544
mobinerr <mobinerr@gmail.com>
2026-07-20 14:19:10 +0330

    feat(product): initialize product details page

---
+++ app/components/common/StateError.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import { PhWarningCircle } from '@/icons';
+
+  defineProps<{
+    message?: string;
+  }>();
+</script>
+
+<template>
+  <div class="flex min-h-96 flex-1 items-center justify-center">
+    <div class="flex flex-col items-center gap-3 text-center">
+      <div class="flex size-12 items-center justify-center rounded-full">
+        <PhWarningCircle :size="64" class="text-red-500" />
+      </div>
+      <h1 class="text-xl font-bold text-red-500">خطایی رخ داد!</h1>
+      <span class="text-sm font-medium text-red-500">
+        {{ message || 'خطایی رخ داده است' }}
+      </span>
+    </div>
+  </div>
+</template>
